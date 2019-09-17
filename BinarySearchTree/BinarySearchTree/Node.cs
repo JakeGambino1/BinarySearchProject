1624ae9452e72793a672500f9861d2e1cfd0a5d2
Jake Gambino <jake.gambino1@gmail.com>
2019-09-17 11:21:33 -0500

    init

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        // member variables
+
+        // constructor
+        public Node()
+        {
+
+        }
+
+        // member methods
+    }
+}
